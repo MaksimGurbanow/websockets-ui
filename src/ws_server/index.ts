3cc2035a57d419cdc968dbc45a84f96919b89a8a
Maksim Gurbanow <maksim20051708@gmail.com>
2024-02-29 22:44:31 +0300

    remove: remove any cases from ws_server\index.ts

--- src/ws_server/index.ts
+++ src/ws_server/index.ts
@@ -21,61 +21,19 @@ const roomsReadyToStart: Set<string> = new Set();
 wss.on("connection", (ws: WebSocket, req) => {
   let currentUser: Player;
   let currentGame: Game | undefined;
-  const { reg, add_user_to_room, update_room, update_winners, attack, create_game, create_room, add_ships, start_game, finish, randomAttack, turn } = Commands
   ws.onmessage = (e: MessageEvent) => {
     try {
       const msg = JSON.parse(e.data.toString());
       const { type, data } = msg;
       const userParseData = parseSafelyData(data);
       switch (type) {
-        case reg:
-          currentUser = createPlayer(userParseData.name, userParseData.password, ws);
-          ws.send(sendJsonMessage(reg, currentUser));
-          ws.send(sendJsonMessage(update_winners, showWinners()));
-          ws.send(sendJsonMessage(update_room, showAvailableRooms(rooms)));
+        case "reg":
+          
           break;
-          case create_room:
-            createRoom({...currentUser});
-            wss.clients.forEach((client) => {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(sendJsonMessage(update_room, showAvailableRooms(rooms)));
-              }
-            })
-            break;
-        case add_ships:
-          currentGame = games.find((game) => 
-            game.roomId === userParseData.gameId);
-          addShips(game, userParseData);
-
-          if (game && game.players && game.players.every((player) => player.ready)) {
-            const currentUserIndex = Math.round(Math.random());
-        
-            game.players.forEach((player) => {
-                if (player.ws) {
-                    player.ws.send(sendJsonMessage(start_game, { ships: player.ships, currentUserIndex: player.index }));
-                    player.ws.send(sendJsonMessage(turn, { currentPlayer: game?.players![currentUserIndex].index }));
-                }
-            });
-          }
-          break
-        case attack:
-          attackFunc(userParseData.currentPlayer, userParseData);
-          break;
-        case randomAttack:
-          attackFunc(userParseData.indexPlayer, userParseData);
-          break;
-          case "add_user_to_room":
-            const { indexRoom } = userParseData;
-            wss.clients.forEach((client) => {
-              if (client.readyState === WebSocket.OPEN) {
-                client.send(sendJsonMessage(update_room, showAvailableRooms(rooms)));
-              }
-            });
-            addUserToRoom(currentUser, indexRoom);
-            break;
+      
         default:
           break;
-        }
+      }
     } catch (error) {
       console.log(error)
   
